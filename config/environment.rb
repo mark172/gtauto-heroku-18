af51adf0b578dd0777bd41e6ccc8bd923774e4a7
mark172 <mark172@me.com>
2021-02-27 11:40:47 -0600

    new mail

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,22 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 
+# ActionMailer::Base.smtp_settings = {
+#     :address => 'smtp.sendgrid.net',
+#     :port => '587',
+#     :authentication => :plain,
+#     :user_name => ENV['SENDGRID_USERNAME'],
+#     :password => ENV['SENDGRID_PASSWORD'],
+#     :domain => 'heroku.com',
+#     :enable_startstls_auto => true
+# }
+
 ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :enable_startstls_auto => true
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'gtauto-heroku-heroku-18.heroku.com',
+  :authentication => :plain,
 }
+ActionMailer::Base.delivery_method = :smtp
